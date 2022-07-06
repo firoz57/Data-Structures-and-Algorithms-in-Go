d68ea177ae634097a7092e15d67ce9af90a1a9a3
firoz47 <97146450+firoz47@users.noreply.github.com>
2022-07-06 20:15:17 +0530

    All operation are done for an array in array.go file

--- Data-Structures/README.md
+++ Data-Structures/README.md
@@ -1,2 +1,3 @@
 This is a Detailed Introduction of Data Structures in Golang.
 Hope you enjoy it and Have Fun.:)
+What we will do in these are: Implementation, Insertion, Deletion and lookup
--- Data-Structures/array.go
+++ Data-Structures/array.go
@@ -15,4 +15,31 @@ func main() {
 	appended_array := make([]int, 10)
 	appended_array = append(array[:], 9)
 	fmt.Print("This is appended_array", appended_array, "\n")
+
+	// Insertion into array
+	arr1 := []int{2, 6, 8, 1}
+	arr1 = append(arr1, 0)
+	copy(arr1[3:], arr1[2:])
+	arr1[2] = 99
+	fmt.Print("This is insertion of 99 in 2nd index: ", arr1, "\n")
+
+	//update of element
+	arr1[4] = 77
+	fmt.Print("This is update of element 77 at index 4: ", arr1, "\n")
+
+	// deletion of an element from an array
+	arr2 := []int{22, 33, 44, 55}
+	i := 2
+
+	fmt.Print("This array before the deletion: ", arr2, "\n")
+
+	copy(arr2[i:], arr2[i+1:])
+	arr2[len(arr2)-1] = 0     // Erase last element and write 0 in its place
+	arr2 = arr2[:len(arr2)-1] // Truncate slice
+
+	fmt.Print("This delete the element from index 2 which is 44: ", arr2, "\n")
+
+	// Lookup at the index in an array
+	i = 2
+	fmt.Print("The element at position 2 in an array is: ", arr2[i], "\n")
 }
